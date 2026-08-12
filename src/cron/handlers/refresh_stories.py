3cac46f95574744d94c7c9a3ea499e752717dbe5
Ian Maze <mazeiandev@outlook.com>
2026-08-11 22:47:45 -0400

    feat(cron): [FB-7] add scheduler to refresh stories

---
+++ src/cron/handlers/refresh_stories.py
@@ -0,0 +1,5 @@
+def refresh_stories():
+    """
+    Refreshes stories by calling the consolidateMessages function.
+    This function is intended to be scheduled to run at specific intervals.
+    """
